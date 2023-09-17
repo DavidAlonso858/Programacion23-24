883538552bf13a347605d6bec30ee549fa2ad84e
david <dalogar0801@g.educaand.es>
2023-09-17 18:43:43 +0200

    prueba

--- 1Trimestre/Tema1/Prueba.java
+++ 1Trimestre/Tema1/Prueba.java
@@ -1,7 +1,22 @@
+//Programa que pida y multiplique dos numeros
+
+import java.util.Scanner;
+
 public class Prueba {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double num1;
+        double num2;
 
-        System.out.println("\n Me llamo David");
+        System.out.println("Me llamo David \n");
+
+        System.out.print("Inserte un numero: ");
+        num1 = sc.nextDouble();
+
+        System.out.print("Inserte otro numero: ");
+        num2 = sc.nextDouble();
+
+        System.out.println("El resultado es: " + num1 * num2);
     }
 
 }
