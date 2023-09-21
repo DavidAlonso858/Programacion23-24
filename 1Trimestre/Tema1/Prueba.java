b3b4abc102e2dc3da0b9f273cd16b60050403a21
DavidAlonso858 <dalogar0801@g.educaand.es>
2023-09-21 10:18:41 +0200

    prueba jueves

--- 1Trimestre/Tema1/Prueba.java
+++ 1Trimestre/Tema1/Prueba.java
@@ -7,14 +7,19 @@ public class Prueba {
         Scanner sc = new Scanner(System.in);
         double num1;
         double num2;
-
+        double numP;
         System.out.println("Me llamo Manuel \n");
+
+        System.out.print("Dime un numero por favor: ");
+        numP = sc.nextDouble();
 
         System.out.print("Inserte un numero: ");
         num1 = sc.nextDouble();
 
         System.out.print("Inserte otro numero: ");
         num2 = sc.nextDouble();
+
+        System.out.println("El numero es: " + numP);
 
         System.out.println("El resultado es: " + num1 * num2);
     }
