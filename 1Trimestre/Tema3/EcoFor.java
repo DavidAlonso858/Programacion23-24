7eaf350f72a1fdc2162e1e4498f947d5e15ce47a
DavidAlonso858 <dalogar0801@g.educaand.es>
2023-10-24 14:44:54 +0200

    prueba

---
+++ 1Trimestre/Tema3/EcoFor.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class EcoFor {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num;
+
+        System.out.println("Introduzca el numero de veces: ");
+
+
+    }
+}
