3cc3afa4c1bb4e7d5508fb4c0d6aa5100a146068
david <dalogar0801@g.educaand.es>
2023-11-03 14:19:19 +0100

    prueba viernes tarde

--- 1Trimestre/Tema3/Refuerzo10.java
+++ 1Trimestre/Tema3/Refuerzo10.java
@@ -3,15 +3,19 @@ import java.util.Scanner;
 public class Refuerzo10 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int num, media = 0, veces = 0;
-        System.out.println("Introduce un numero negativo cuando no quieras introducir mas");
+        int num, todos = 0, media = 0, veces = 0;
+
+        System.out.println("Introduce un numero negativo si quieres parar");
+
         do {
             System.out.print("Introduzca un numero: ");
             num = sc.nextInt();
-            veces++;
-            num+=num;
-            media = num / veces;
-        } while (num > 0);
+            if (num >= 0) {
+                veces++;
+                todos += num;
+            }
+        } while (num >= 0);
+        media = todos / veces;
         System.out.println("La media es: " + media);
     }
 }
