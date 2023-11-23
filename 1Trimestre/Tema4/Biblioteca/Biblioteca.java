fdb8ac18570f4f0435aa4e440f0c515f82b3bf4d
DavidAlonso858 <dalogar0801@g.educaand.es>
2023-11-23 11:10:58 +0100

    prueba jueves

--- 1Trimestre/Tema4/Biblioteca/Biblioteca.java
+++ 1Trimestre/Tema4/Biblioteca/Biblioteca.java
@@ -15,15 +15,8 @@ public class Biblioteca {
 
     public static boolean esCapicua(int numero) {
         boolean capicua;
-        int numReverso = 0;
 
-        int aux = numero;
-        while (aux > 0) {
-            int cifra = aux % 10;
-            numReverso = (numReverso * 10) + cifra;
-            aux /= 10;
-        }
-        if (numReverso == numero) {
+        if (voltea(numero) == numero) {
             capicua = true;
         } else {
             capicua = false;
@@ -62,13 +55,92 @@ public class Biblioteca {
         return longitud;
     }
 
+    public static int voltea(int numero) {
+        int cifra, numVolteado = 0;
+        int aux = numero;
+        while (aux > 0) {
+            cifra = aux % 10;
+            numVolteado = numVolteado * 10 + cifra;
+            aux /= 10;
+        }
+        return numVolteado;
+    }
+
+    public static int digitoN(int numero, int posicion) {
+        int digito, aux = voltea(numero), digitoBueno = 0;
+        boolean encontrado = false;
+
+        for (int contPosicion = 0; contPosicion < digitos(numero) && !encontrado; contPosicion++) {
+            digito = aux % 10;
+            if (contPosicion == posicion) {
+                digitoBueno = digito;
+                encontrado = true;
+            }
+            aux /= 10;
+        }
+        return digitoBueno;
+    }
+
+    public static int posicionDigito(int numero, int digito) {
+        int cifra, aux = voltea(numero), posicionBuena = 0;
+        boolean encontrado = false;
+
+        for (int contPosicion = 0; contPosicion < digitos(numero) && !encontrado; contPosicion++) {
+            cifra = aux % 10;
+            if (cifra == digito) {
+                posicionBuena = contPosicion;
+                encontrado = true;
+            } else {
+                posicionBuena = -1;
+            }
+            aux /= 10;
+        }
+        return posicionBuena;
+    }
+
+    public static int quitaPorDetras(int numero, int detrasDigitos) {
+        int aux = numero, nuevoNumero = 0;
+        boolean encontrado = false;
+
+        for (int contPosicion = 0; contPosicion < digitos(numero) && !encontrado; contPosicion++) {
+            if (contPosicion == detrasDigitos) {
+                nuevoNumero = aux;
+                encontrado = true;
+            }
+
+            aux /= 10;
+        }
+        return nuevoNumero;
+    }
+
+    public static int quitaPorDelante(int numero, int delanteDigitos) {
+        int aux = voltea(numero), nuevoNumero = 0;
+        boolean encontrado = false;
+
+        for (int contPosicion = 0; contPosicion < digitos(numero) && !encontrado; contPosicion++) {
+            if (contPosicion == delanteDigitos) {
+                nuevoNumero = aux;
+                encontrado = true;
+            }
+
+            aux /= 10;
+        }
+        return nuevoNumero;
+    }
+
     public static void main(String[] args) {
-        int numero = 17, base = 2, exponente = 3;
+        int numero = 17, base = 2, exponente = 3, posicion = 0, digito = 1,
+                detrasDigitos = 1, delanteDigitos=1;
 
         System.out.println("Es primo: " + esPrimo(numero));
         System.out.println("Es capicua: " + esCapicua(numero));
         System.out.println("El siguiente primo es:" + siguientePrimo(numero));
         System.out.println(potencia(base, exponente));
         System.out.println("El numero de digitos es: " + digitos(numero));
+        System.out.println("Volteado es: " + voltea(numero));
+        System.out.println(digitoN(numero, posicion));
+        System.out.println(posicionDigito(numero, digito));
+        System.out.println("Al quitarle por detras se queda: "+quitaPorDetras(numero, detrasDigitos));
+        System.out.println("Al quitarle por delante se queda: "+quitaPorDelante(numero, delanteDigitos));
     }
 }
