7015e650b0842548e99f022085090abee1896451
DavidAlonso858 <dalogar0801@g.educaand.es>
2023-11-28 14:43:41 +0100

    prueba martes

---
+++ 1Trimestre/Tema5/Array.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Array {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    
+        int prueba[] = new int[10];
+        System.out.println(prueba);
+        
+    }
+}
