1ec1bde2cda84406f18da7d189d5a4b55ae6f7d3
DavidAlonso858 <dalogar0801@g.educaand.es>
2024-01-10 14:37:38 +0100

    refuerzo final

--- 1Trimestre/Tema5/Refuerzo15.java
+++ 1Trimestre/Tema5/Refuerzo15.java
@@ -22,50 +22,62 @@ public class Refuerzo15 {
         return mesa;
     }
 
+    public static boolean mesaSinCompartir(int[] mesa, int numero) {
+        boolean encontrado = false;
+        for (int i = 0; i < mesa.length && !encontrado; i++) {
+            if (mesa[i] == 0) {
+                mesa[i] = numero;
+                encontrado = true;
+                System.out.println("Por favor, sientense en la mesa numero " + (i + 1));
+            }
+        }
+        return encontrado;
+    }
+
+    public static void mesaCompartida(int[] mesa, int numero) {
+        boolean encontrado = false;
+        for (int i = 0; i < mesa.length && !encontrado; i++) {
+            if (mesa[i] + numero <= 4) {
+                mesa[i] += numero;
+                encontrado = true;
+                System.out.println(
+                        "Tendrán que compartir mesa. Por favor, sientese en la mesa numero " + (i + 1));
+            } else if (mesa[i] + numero > 4 && i == mesa.length - 1) {
+                System.out.println("Los siento, en estos momentos no queda sitio.");
+            }
+        }
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int[] mesa = new int[10];
-        int numero;
         boolean encontrado;
+        int numero;
         System.out.print("Bienvenido a Restaurante Array\n");
 
-        int[] ocupacion = rellenarMesa(mesa); // solo una vez y luego se modifica con lo introducido
+        rellenarMesa(mesa); // solo una vez y luego se modifica con lo introducido
         do {
             System.out.println();
             System.out.print("Mesa nª ");
             mostrarIndiceMesa(mesa);
 
-            System.out.print("\nOcupacion: " + Arrays.toString(ocupacion));
+            System.out.print("\nOcupacion: " + Arrays.toString(mesa));
 
             System.out.print("\n\n¿Cuantos son? (Introduzca -1 para salir del programa): ");
             numero = sc.nextInt();
-            if (numero > 4) {
+            if (numero > 4 || numero == 0) {
                 System.out.println("Los siento no admitimos grupos de " + numero
                         + ", haga grupos de 4 personas como maximo e intente de nuevo.");
-            } else {
-                encontrado = false;
-                for (int i = 0; i < ocupacion.length && !encontrado && numero > 0; i++) {
-                    if (ocupacion[i] == 0) {
-                        ocupacion[i] = numero;
-                        encontrado = true;
-                        System.out.println("Por favor, sientense en la mesa numero " + (i + 1));
-                    }
-                }
+            } else if (numero != -1) {
+                encontrado = mesaSinCompartir(mesa, numero); // pongo el bucle como funcion que devuelva el mensaje y
+                                                             // booleano (boolean)
+
                 if (!encontrado) {
-                    for (int i = 0; i < ocupacion.length && !encontrado && numero > 0; i++) {
-                        if (ocupacion[i] + numero <= 4) {
-                            ocupacion[i] = numero;
-                            encontrado = true;
-                            System.out.println(
-                                    "Tendrán que compartir mesa. Por favor, sientese en la mesa numero " + (i + 1));
-                        } else if (ocupacion[i] + numero > 4 && i == ocupacion.length - 1) {
-                            System.out.println("Los siento, en estos momentos no queda sitio.");
-                        }
-                    }
+                    mesaCompartida(mesa, numero); // devuelve solo el mensaje (void)
                 }
             }
 
-        } while (numero > 0);
+        } while (numero != -1);
         System.out.println("Gracias. Hasta pronto.");
     }
 }
