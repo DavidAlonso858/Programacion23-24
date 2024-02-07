fce685eaaf7fd8162d1594e3f44f611b15046b38
david <dalogar0801@g.educaand.es>
2024-02-07 11:44:49 +0100

    Ahorcado (lio con el array de letras)

--- 2 Trimestre/Tema6/Ejercicio612Ahorcado.java
+++ 2 Trimestre/Tema6/Ejercicio612Ahorcado.java
@@ -1,40 +1,6 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Ejercicio612Ahorcado {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String contraseña = "Patata";
-        char letrAhorcado = '_';
-        int intentos = 7, longitud = contraseña(contraseña);
-        char[] muestra = new char[longitud];
-
-        for (int i = 0; i < muestra.length; i++) {
-            muestra[i] = '_';
-        }
-
-        Jugador1(contraseña, longitud, letrAhorcado, muestra);
-        while (intentos > 0 && !acertado(muestra)) {
-            System.out.print("\nIntroduzca la letra para adivinar: ");
-            letrAhorcado = sc.nextLine().charAt(0);
-            
-            muestra = Jugador1(contraseña, longitud, letrAhorcado, muestra);
-            intentos--;
-            System.out.println("Te quedan " + intentos + " intentos");
-        }
-
-        if (intentos > 0 && acertado(muestra)) {
-            System.out.println("\n¡Enhorabuena. Has acertado.!");
-        } else {
-            System.out.println("\n¡Perdiste. Suerte la proxima.!");
-        }
-    }
-
-    public static int contraseña(String contraseña) {
-        int longitud = contraseña.length();
-
-        return longitud;
-    }
 
     public static char[] Jugador1(String contraseña, int longitud, char letrAhorcado, char[] muestra) {
         contraseña = contraseña.toLowerCase();
@@ -59,5 +25,33 @@ public class Ejercicio612Ahorcado {
             }
         }
         return acertado;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String contraseña = "Malaga";
+        char letrAhorcado = '_';
+        int intentos = 7, longitud = contraseña.length();
+        char[] muestra = new char[longitud];
+
+        for (int i = 0; i < muestra.length; i++) {
+            muestra[i] = '_';
+        }
+
+        Jugador1(contraseña, longitud, letrAhorcado, muestra);
+        while (intentos > 0 && !acertado(muestra)) {
+            System.out.print("\nIntroduzca la letra para adivinar: ");
+            letrAhorcado = sc.nextLine().charAt(0);
+
+            muestra = Jugador1(contraseña, longitud, letrAhorcado, muestra);
+            intentos--;
+            System.out.println("Te quedan " + intentos + " intentos");
+        }
+
+        if (intentos > 0 && acertado(muestra)) {
+            System.out.println("\n¡Enhorabuena. Has acertado.!");
+        } else {
+            System.out.println("\n¡Perdiste. Suerte la proxima.!");
+        }
     }
 }
