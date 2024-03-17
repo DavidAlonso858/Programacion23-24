5931bb826ccd8dd153904b272b1bafc35fd1a7ed
david <dalogar0801@g.educaand.es>
2024-03-17 19:23:20 +0100

    Ejercicio caja con cambio etiqueta (maximo 30 carac), dimension volumen y mostrar atributos

--- 2 Trimestre/Tema8 POO Herencia/Ejercicio812EmpresaTransporte/src/App.java
+++ 2 Trimestre/Tema8 POO Herencia/Ejercicio812EmpresaTransporte/src/App.java
@@ -4,7 +4,7 @@ public class App {
         Caja cj1 = new Caja(50, 30, 20, u1);
 
         cj1.setEtiqueta();
-        
+        System.out.println("El volumen en m3: " + cj1.getVolumen());
         System.out.println(cj1.toString());
 
     }
