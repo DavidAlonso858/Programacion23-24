2b9417032a6f5071018424734d060c627398fdda
david <dalogar0801@g.educaand.es>
2024-03-18 15:08:56 +0100

    Lista con metodos de arraysCopy

---
+++ 2 Trimestre/Tema8 POO Herencia/Ejercicio814ListaToString/src/Lista.java
@@ -0,0 +1,55 @@
+import java.util.Arrays;
+
+public class Lista {
+    Integer[] tabla;
+
+    public Lista() {
+        tabla = new Integer[0];
+    }
+
+    public void insertarPrincipio(Integer nuevo) {
+        tabla = Arrays.copyOf(tabla, tabla.length + 1);
+        System.arraycopy(tabla, 0, tabla, 1, tabla.length - 1);
+        tabla[0] = nuevo;
+    }
+
+    public void insertarFinal(Integer nuevo) {
+        tabla = Arrays.copyOf(tabla, tabla.length + 1);
+        tabla[tabla.length - 1] = nuevo;
+    }
+
+    public void insertarFinal(Lista otraLista) {
+        int tamIni = tabla.length;
+        tabla = Arrays.copyOf(tabla, tabla.length + otraLista.tabla.length);
+        System.arraycopy(otraLista.tabla, 0, tabla, tamIni, otraLista.tabla.length);
+    }
+
+    public void insertar(int posicion, Integer nuevo) {
+        tabla = Arrays.copyOf(tabla, tabla.length + 1);
+        System.arraycopy(tabla, posicion, tabla, posicion + 1, tabla.length - posicion - 1);
+        tabla[posicion] = nuevo;
+    }
+
+    public Integer get(int indice) {
+        Integer resultado = null;
+        if (indice >= 0 && indice < tabla.length) {
+            resultado = tabla[indice];
+        }
+        return resultado;
+    }
+
+    public int buscar(Integer claveBusqueda) {
+        int indice = -1;
+        for (int i = 0; i < tabla.length && indice == -1; i++) {
+            if (tabla[i].equals(claveBusqueda)) {
+                indice = i;
+            }
+        }
+        return indice;
+    }
+
+    @Override
+    public String toString() { // metodo toString en vez de mostrar
+        return "Lista [tabla= " + Arrays.toString(tabla) + "]";
+    }
+}
