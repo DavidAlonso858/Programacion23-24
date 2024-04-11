aceb6fac2f404a2cecbb6a8565b28449f1611376
david <dalogar0801@g.educaand.es>
2024-04-11 20:26:04 +0200

    Ejercicio abstracto base figuras

---
+++ 3 Trimestre/Tema8 POO Herencia/EjemploClasesAbstracto/src/ejemploclasesabstracto/Cuadrado.java
@@ -0,0 +1,63 @@
+package ejemploclasesabstracto;
+
+public class Cuadrado extends FiguraGeometrica{
+    
+    private Rectangulo rect;
+    
+
+    public Cuadrado (double lado, String color){
+        super(color);
+        if (lado > 0)
+            rect = new Rectangulo(lado,lado,color);
+        else
+            rect = new Rectangulo(1,1,color);
+    }
+    
+    public double getLado(){
+        return rect.getAltura();
+    }
+    
+    public void setLado(double lado){
+        rect.setAltura(lado);
+        rect.setBase(lado);
+    }
+    
+    
+    @Override
+    public double area() {
+        return rect.area();
+    }
+
+    @Override
+    public double perimetro() {
+        return rect.perimetro();
+    }
+    
+    public String toString(){
+        String cadena = "";
+        
+        cadena += "\n --CUADRADO--";
+        cadena += "\nLado: " + this.getLado();
+        cadena += super.toString();
+        
+        return cadena; 
+    }
+    
+    
+    public void pintar(){
+        
+        for (int fila = 0; fila < this.getLado(); fila++){
+            for (int col = 0; col < this.getLado(); col++) {
+                System.out.print("#");
+            }
+            System.out.println();
+        }
+    }
+    
+    
+    
+    
+    
+    
+    
+}
