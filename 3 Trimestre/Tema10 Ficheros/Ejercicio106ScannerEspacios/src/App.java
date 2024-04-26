72946717326e29636e3ed851049ce08d292e8e82
DavidAlonso858 <dalogar0801@g.educaand.es>
2024-04-26 11:15:35 +0200

    Ejercicio 106

---
+++ 3 Trimestre/Tema10 Ficheros/Ejercicio106ScannerEspacios/src/App.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        BufferedReader in = null;
+        String linea = "";
+
+        double suma = 0, contador = 0, num;
+
+        try {
+            in = new BufferedReader(new FileReader("Enteros.txt"));
+            linea = in.readLine();
+            while (linea != null) {
+                sc = new Scanner(linea).useLocale(Locale.US);
+                if (sc.hasNextDouble()) {
+                    num=sc.nextDouble();
+                    suma += num;
+                    contador++;
+                }
+                linea = in.readLine();
+            }
+            System.out.println("Suma: " + suma);
+            System.out.println("Media: " + (suma / contador));
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        } finally {
+            if (in != null) {
+                try {
+                    in.close();
+                } catch (IOException ex) {
+                    System.out.println(ex);
+                }
+            }
+        }
+        System.out.println(linea);
+    }
+}
