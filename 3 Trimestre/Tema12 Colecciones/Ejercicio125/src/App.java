7c6e9105a3c56866b8c66aaaccb273d947b257d4
david <dalogar0801@g.educaand.es>
2024-05-07 17:44:48 +0200

    Ejercicio 125 .contains() para no repetir en la lista

--- 3 Trimestre/Tema12 Colecciones/Ejercicio125/src/App.java
+++ 3 Trimestre/Tema12 Colecciones/Ejercicio125/src/App.java
@@ -1,5 +1,35 @@
+import java.util.Collection;
+import java.util.LinkedList;
+
 public class App {
-    public static void main(String[] args)  {
-        
+
+    public static int aleatorio20() {
+        int numero;
+
+        numero = (int) (Math.random() * (11 - 1) + 1);
+
+        return numero;
+    }
+
+    public static void main(String[] args) {
+        Collection<Integer> numerosAleatorios = new LinkedList<>();
+
+        for (int i = 0; i < 20; i++) {
+            numerosAleatorios.add(aleatorio20());
+        }
+
+        System.out.println("LISTA CON REPETICIONES");
+        System.out.println(numerosAleatorios);
+
+        Collection<Integer> noRepetidos = new LinkedList<>();
+
+        for (Integer i : numerosAleatorios) {
+            if (!noRepetidos.contains(i)) { // false si no lo contiene para que no añada repetidos
+                noRepetidos.add(i);
+            }
+        }
+
+        System.out.println("LISTA SIN REPETICIONES");
+        System.out.println(noRepetidos);
     }
 }
