853d4a62821725ce9edb3ceb90b4b38a1aaca8be
david <dalogar0801@g.educaand.es>
2024-05-09 17:19:26 +0200

    Serializable, Comparable (compareTo), TreeSet y equals (this.dni.equals())

---
+++ 3 Trimestre/Tema12 Colecciones/Ejercicio1210Todo/src/ejercicio1210todo/App.java
@@ -0,0 +1,112 @@
+package ejercicio1210todo;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class App {
+    public static int menu(Scanner sc) {
+        int opcion;
+        System.out.println("\n----MENU----");
+
+        System.out.println("1. Alta nuevo socio");
+        System.out.println("2. Baja de socio");
+        System.out.println("3. Modificacion de socio");
+        System.out.println("4. Listado por DNI");
+        System.out.println("5. Listado por antiguedad");
+        System.out.println("6. Salir");
+
+        System.out.print("Introduzca la opcion deseada: ");
+        opcion = sc.nextInt();
+
+        return opcion;
+    }
+
+    public static boolean alta(Scanner sc, Set<Socio> conjuntoSocios) {
+        String dni, nombre, fechaAlta;
+        System.out.print("Introduzca DNI del nuevo socio: ");
+        dni = sc.next();
+        sc.nextLine();
+
+        System.out.print("Introduzca NOMBRE del nuevo socio: ");
+        nombre = sc.nextLine();
+
+        System.out.print("Introduzca FECHA ALTA del nuevo socio (dd-MM-yyy): ");
+        fechaAlta = sc.nextLine();
+
+        Socio s = new Socio(dni, nombre, fechaAlta);
+
+        return conjuntoSocios.add(s);
+    }
+
+    public static boolean modificacion(Scanner sc, Set<Socio> conjuntoSocios) {
+        String dni, nombre, fechaAlta;
+
+        System.out.print("Introduzca DNI del modificado: ");
+        dni = sc.next();
+        sc.nextLine();
+
+        System.out.print("\nIntroduzca NOMBRE del modificado: ");
+        nombre = sc.nextLine();
+
+        System.out.print("\nIntroduzca FECHA ALTA modifcado (dd-MM-yyy): ");
+        fechaAlta = sc.nextLine();
+
+        Socio s = new Socio(dni, nombre, fechaAlta);
+
+        return conjuntoSocios.add(s);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int opcion;
+        String dni;
+        Set<Socio> conjuntoSocios = new TreeSet<>();
+
+        try (ObjectInputStream in = new ObjectInputStream(new FileInputStream("socios.dat"))) {
+            conjuntoSocios = (TreeSet<Socio>) in.readObject(); // c casteo del tipo al leelo
+
+        } catch (IOException | ClassNotFoundException ex) {
+            System.out.println(ex.getMessage());
+        }
+
+        do {
+            opcion = menu(sc);
+            switch (opcion) {
+                case 1 -> alta(sc, conjuntoSocios);
+                case 2 -> {
+                    System.out.print("Introduzca el dni que quiere darse de baja: ");
+                    dni = sc.next();
+                    sc.nextLine();
+                    Socio s = new Socio(dni);
+                    conjuntoSocios.remove(s);
+                }
+                case 3 -> {
+                    System.out.print("Introduzca el dni del socio que quiere modificar: ");
+                    dni = sc.nextLine();
+                    Socio s = new Socio(dni);
+                    conjuntoSocios.remove(s);
+
+                    modificacion(sc, conjuntoSocios);
+                }
+                case 4 -> System.out.println(conjuntoSocios);
+                case 5 -> { // orden de mas antiguo
+                    Set<Socio> ordenAntiguedad = new TreeSet<>((s1, s2) -> s2.antiguedad() - s1.antiguedad());
+                    ordenAntiguedad.addAll(conjuntoSocios);
+                    System.out.println(ordenAntiguedad);
+                }
+                case 6 -> System.out.println("Espero que todo haya ido bien!");
+            }
+        } while (opcion != 6);
+        try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("socios.dat"))) {
+            out.writeObject(conjuntoSocios);
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+}
---
+++ 3 Trimestre/Tema12 Colecciones/Ejercicio1210Todo/src/ejercicio1210todo/Socio.java
@@ -0,0 +1,62 @@
+package ejercicio1210todo;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.time.temporal.ChronoUnit;
+
+public class Socio implements Serializable, Comparable<Socio> {
+    private String dni;
+    private String nombre;
+    private LocalDate fechaAlta;
+
+    public Socio(String dni, String nombre, String fechaAlta) {
+        this.dni = dni;
+        this.nombre = nombre;
+        DateTimeFormatter f = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        this.fechaAlta = LocalDate.parse(fechaAlta, f);
+    }
+
+    public Socio(String dni) {
+        this.dni = dni;
+    }
+
+    @Override
+    public int compareTo(Socio o) {
+
+        return this.dni.compareTo(o.dni);
+    }
+
+    public int antiguedad() {
+        int antiguedad = (int) fechaAlta.until(LocalDate.now(), ChronoUnit.YEARS);
+
+        return antiguedad;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        Socio s = (Socio) obj;
+
+        return this.dni.equals(s.dni);
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public LocalDate getFechaAlta() {
+        return fechaAlta;
+    }
+
+    @Override
+    public String toString() {
+        DateTimeFormatter f = DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL);
+        return "\nSocio [dni-> " + this.dni + ", nombre-> " + nombre + ", fechaAlta->" + f.format(fechaAlta) + "]";
+    }
+
+}
