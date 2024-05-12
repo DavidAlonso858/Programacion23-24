ab0649df6e4ea007f9ed08db4846b342c3fce4fa
david <dalogar0801@g.educaand.es>
2024-05-12 13:52:32 +0200

    Unir dos listas en una y ordenadas Collections.sort(lista1) o lista1.sort(null)

--- 3 Trimestre/Tema12 Colecciones/Refuerzo24Union/src/ejercicio1224union/Ejercicio1224Union.java
+++ 3 Trimestre/Tema12 Colecciones/Refuerzo24Union/src/ejercicio1224union/Ejercicio1224Union.java
@@ -1,7 +1,38 @@
 package ejercicio1224union;
+
+import java.util.Collections;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Ejercicio1224Union {
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static int aleatorio() {
+        int n;
+
+        n = (int) (Math.random() * (50));
+
+        return n;
     }
-    
+
+    public static void main(String[] args) {
+        List<Integer> lista1 = new LinkedList<>();
+        List<Integer> lista2 = new LinkedList<>();
+
+        while (lista1.size() < 5 && lista2.size() < 5) {
+            lista1.add(aleatorio());
+            lista2.add(aleatorio());
+        }
+
+        Collections.sort(lista1);
+        lista2.sort(null);
+
+        List<Integer> listaFinal = new LinkedList<>(lista1); // le paso la primera y añado la segunda
+
+        listaFinal.addAll(lista2);
+
+        listaFinal.sort(null);
+
+        System.out.println(listaFinal);
+        System.out.println(lista1 + " y " + lista2);
+    }
 }
