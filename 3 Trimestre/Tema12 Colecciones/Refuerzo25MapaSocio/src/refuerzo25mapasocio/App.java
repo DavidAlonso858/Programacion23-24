a728b345cae15c1aa9b1c6c1107d530f5d8c0d07
david <dalogar0801@g.educaand.es>
2024-05-14 20:42:02 +0200

    Mapa de String y Soios ESTUDIAR

--- 3 Trimestre/Tema12 Colecciones/Refuerzo25MapaSocio/src/refuerzo25mapasocio/App.java
+++ 3 Trimestre/Tema12 Colecciones/Refuerzo25MapaSocio/src/refuerzo25mapasocio/App.java
@@ -1,14 +1,26 @@
 package refuerzo25mapasocio;
 
-import java.util.LinkedHashMap;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeMap;
 
 public class App {
     static Scanner sc = new Scanner(System.in);
 
     public static int menu() {
         int opcion;
+        System.out.println("\n--- MENU ---");
 
         System.out.println("1. Alta socio");
         System.out.println("2. Baja socio");
@@ -20,25 +32,156 @@ public class App {
 
         System.out.print("Introduzca la opcion: ");
         opcion = sc.nextInt();
+        sc.nextLine();
 
         return opcion;
 
     }
 
+    public static void alta(Map<String, Socio> club) {
+        String apodo, nombre, fechaIngreso;
+
+        System.out.print("\nIntroduzca el apodo del nuevo socio: ");
+        apodo = sc.nextLine();
+
+        System.out.print("Introduzca el nombre del nuevo socio: ");
+        nombre = sc.nextLine();
+
+        System.out.print("Introduzca la fecha de ingreso (dd/MM/yyyy): ");
+        fechaIngreso = sc.nextLine();
+
+        club.put(apodo, new Socio(apodo, nombre, fechaIngreso));
+    }
+
+    public static void baja(Map<String, Socio> club) {
+        String apodoEliminar;
+
+        System.out.print("\nIntroduzca el apodo del socio para darse de baja: ");
+        apodoEliminar = sc.nextLine();
+
+        club.remove(apodoEliminar);
+    }
+
+    public static void modifacion(Map<String, Socio> club, String apodoModi) {
+        String nombre = "", fechaIngreso = "";
+        Socio s1 = club.get(apodoModi); // para modificar el socio con esa clave
+        int opcion;
+
+        do {
+            System.out.println("\n-- PERIODO MODIFICACION--");
+            System.out.println("1. Modificar nombre");
+            System.out.println("2. Modificar fecha de ingreso");
+            System.out.println("3. No modificar nada mas");
+
+            System.out.print("Introduzca la opcion a modificar: ");
+            opcion = sc.nextInt();
+            sc.nextLine();
+
+            switch (opcion) {
+                case 1 -> {
+                    System.out.print("\nIntroduzca nuevo nombre: ");
+                    nombre = sc.nextLine();
+                    s1.setNombre(nombre); // modifco eso manteniendo lo demas
+                }
+                case 2 -> {
+                    System.out.print("\nIntroduzca nueva fecha de ingreso (dd/MM/yyyy): ");
+                    fechaIngreso = sc.nextLine();
+                    s1.setFechaIngreso(fechaIngreso); // modifco eso manteniendo lo demas
+                }
+                case 3 -> {
+                    System.out.println("Tenga un buen dia caballero/señorita!");
+                }
+            }
+        } while (opcion != 3);
+    }
+
+    public static List<Socio> ordenAño(Map<String, Socio> club, int año) {
+        Set<Map.Entry<String, Socio>> ordenAño = club.entrySet();
+
+        Iterator<Map.Entry<String, Socio>> it = ordenAño.iterator(); // iterator Map.Entry
+
+        while (it.hasNext()) {
+            Map.Entry<String, Socio> mapEntry = it.next();
+            if (mapEntry.getValue().getFechaIngreso().getYear() > año) { // elimina para ordenar los anteriores a año
+                it.remove();
+            }
+        }
+
+        Collection<Socio> listaOrdenAño = club.values(); // pasa los valores de Mapa con lo eliminado
+        List<Socio> listaOrdenAñoFinal = new LinkedList<>(listaOrdenAño); // pasa a List para usar el sort
+
+        Collections.sort(listaOrdenAñoFinal,
+                (s1, s2) -> s1.getFechaIngreso().getYear() - s2.getFechaIngreso().getYear());
+
+        return listaOrdenAñoFinal;
+    }
+
     public static void main(String[] args) {
         int opcion;
-        Map<String, Socio> club = new LinkedHashMap<>();
-        
+        // Las claves en los mapas son unicas
+        Map<String, Socio> club = new TreeMap<>(); // TreeMap orden clave sin necesidad de compareTo
+
+        try (ObjectInputStream in = new ObjectInputStream(new FileInputStream("club.dat"))) {
+            club = (TreeMap<String, Socio>) in.readObject(); // se castea el tipo para leer
+
+        } catch (IOException | ClassNotFoundException ex) {
+            System.out.println(ex.getMessage());
+        }
+
         do {
             opcion = menu();
             switch (opcion) {
-                case value:
+                case 1 -> {
+                    alta(club);
+                }
+                case 2 -> {
+                    baja(club);
+                }
 
-                    break;
+                case 3 -> {
+                    String apodoModi;
+                    do {
 
-                default:
-                    break;
+                        System.out.print("\nIntroduzca el apodo del socio a modificar: ");
+                        apodoModi = sc.nextLine();
+                        club.containsKey(apodoModi);
+
+                    } while (!club.containsKey(apodoModi));
+
+                    modifacion(club, apodoModi);
+
+                }
+                case 4 -> {
+                    System.out.println("Orden alfabetico -> " + club);
+                }
+                case 5 -> {
+                    Collection<Socio> clubAntiguedad = club.values(); // pasa los valores de Mapa
+                    List<Socio> listaClubAntiguedad = new LinkedList<>(clubAntiguedad); // pasa esa Collection a List
+                                                                                        // para usar el sort
+
+                    Collections.sort(listaClubAntiguedad, (s1, s2) -> s2.antiguedad() - s1.antiguedad());
+                    // de mayor a menor
+                    System.out.println("Orden de antiguedad -> " + listaClubAntiguedad);
+                }
+                case 6 -> {
+                    int año;
+                    System.out.print("Hasta que año quiere la lista de socios: ");
+                    año = sc.nextInt();
+                    System.out.println("Orden con alta anterior a " + año + "-> " + ordenAño(club, año));
+
+                }
+                case 7 -> {
+                    System.out.println("Espero que haya ido todo bien!");
+                }
+
             }
         } while (opcion != 7);
+
+        try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("club.dat"))) {
+            out.writeObject(club);
+
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
     }
 }
--- 3 Trimestre/Tema12 Colecciones/Refuerzo25MapaSocio/src/refuerzo25mapasocio/Socio.java
+++ 3 Trimestre/Tema12 Colecciones/Refuerzo25MapaSocio/src/refuerzo25mapasocio/Socio.java
@@ -3,6 +3,8 @@ package refuerzo25mapasocio;
 import java.io.Serializable;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.time.temporal.ChronoUnit;
 
 public class Socio implements Serializable {
     private String apodo;
@@ -14,6 +16,54 @@ public class Socio implements Serializable {
         this.nombre = nombre;
         DateTimeFormatter f = DateTimeFormatter.ofPattern("dd/MM/yyyy");
         this.fechaIngreso = LocalDate.parse(fechaIngreso, f);
+    }
+
+    public Socio(String nombre) { // constructor para trabajar en modi
+        this.nombre = nombre;
+    }
+
+    public Socio(String apodo, String fechaIngreso) { // constructor para trabajar en modi
+        DateTimeFormatter f = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        this.fechaIngreso = LocalDate.parse(fechaIngreso, f);
+    }
+
+    public int antiguedad() {
+        int antiguedad;
+        // desde la ingresada hasta ahora en años
+        antiguedad = (int) this.fechaIngreso.until(LocalDate.now(), ChronoUnit.YEARS);
+
+        return antiguedad;
+    }
+
+    public void setApodo(String apodo) {
+        this.apodo = apodo;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setFechaIngreso(String fechaIngreso) {
+        DateTimeFormatter f = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        this.fechaIngreso = LocalDate.parse(fechaIngreso, f);
+    }
+
+    public String getApodo() {
+        return apodo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public LocalDate getFechaIngreso() {
+        return fechaIngreso;
+    }
+
+    @Override
+    public String toString() {
+        DateTimeFormatter f = DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL);
+        return " Socio [apodo-> " + apodo + ", nombre->" + nombre + ", fechaIngreso->" + f.format(fechaIngreso) + " ]";
     }
 
 }
